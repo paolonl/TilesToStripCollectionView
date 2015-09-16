fc99eb20e03703bb31986d0b3cb653d6a7a7a818
PaoloNL <moronipa@gmail.com>
2015-09-16 22:14:24 +0200

    First commit

---
+++ CustomFlowLayout/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  CustomFlowLayout
+//
+//  Created by Paolo Moroni on 16/09/15.
+//  Copyright (c) 2015 none. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var numberLabel: UILabel!
+}
--- CustomFlowLayout/ViewController.swift
+++ CustomFlowLayout/ViewController.swift
@@ -8,10 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    @IBOutlet weak var topConstraint: NSLayoutConstraint!
+    @IBOutlet weak var collectionView: UICollectionView?
+    private let tilesFlowLayout = UICollectionViewFlowLayout()
+    private let stripFlowLayout = UICollectionViewFlowLayout()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tilesFlowLayout.scrollDirection = UICollectionViewScrollDirection.Vertical
+        self.tilesFlowLayout.itemSize = CGSizeMake(125, 125)
+        self.tilesFlowLayout.minimumInteritemSpacing = 0
+        
+        self.stripFlowLayout.scrollDirection = UICollectionViewScrollDirection.Horizontal
+        self.stripFlowLayout.itemSize = CGSizeMake(125, 125)
+        self.tilesFlowLayout.minimumInteritemSpacing = 0
+
+        self.collectionView?.collectionViewLayout = tilesFlowLayout
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,5 +37,24 @@ class ViewController: UIViewController {
     }
 
 
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12;
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell: CollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as! CollectionViewCell
+        cell.numberLabel.text = String(indexPath.row)
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+        self.topConstraint.constant = CGRectGetMaxY(self.collectionView!.bounds) - 125
+        UIView.animateWithDuration(0.6, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+            self.view.layoutIfNeeded()
+            self.collectionView?.collectionViewLayout = self.stripFlowLayout
+            }, completion: { (finished) -> Void in
+        })
+    }
 }
 
